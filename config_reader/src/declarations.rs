09c0036878478ff2ee206526865d3b47be84fd8d
JohnnyHuang101 <h.johnny@wustl.edu>
2025-09-01 20:00:42 -0500

    adding more steps, folloiwng his instructions

--- config_reader/src/declarations.rs
+++ config_reader/src/declarations.rs
@@ -1,2 +1,17 @@
-//define the error code
-const BAD_ARGS: u8 = 1
+
+use std::sync::atomic::AtomicBool;
+
+type Play = Vec<(usize,String,String)>; // line#, player, line
+
+const MIN_ARGS: usize =2;
+const MAX_ARGS: usize =3; // includes extra complaints
+
+const PROGRAM_NAME: usize =0;
+const CONFIG_FILE: usize =1;
+const WHINGE_STATUS: usize =2;
+
+const BAD_ARGS: u8 = 8;
+const BAD_COMMAND_LINE_ARGS: u8 = 1;
+const GENERATION_FAILURE: u8 = 1;
+
+static WHINGE: AtomicBool = AtomicBool::new(false);
--- config_reader/src/main.rs
+++ config_reader/src/main.rs
@@ -4,17 +4,39 @@ use std::process::ExitCode;
 // add atomic stuff
 
 
+fn usage(program_name: &String) {
+    println!("usage: {} <configuration_file_name> [whinge]", program_name);
+}
 
-fn main() -> Result<(), u8> {
-    let args: Vec<String> = env::args().collect();
-    
-    //helpful message for wrong usage
-    if args.len()<2{
-        println!("Usage: {} <config_file> || Only takes 1 ")
-        return return Err(BAD_ARGS);
+
+fn parse_args(config: &mut String) -> Result<(), u8> {
+
+    let mut args: Vec<String> = Vec::new();
+
+    // push each argument string into the vector
+    for arg in env::args() {
+        args.push(arg);
     }
 
-    let config = &args[1]
+    if !(args.len() == 2 || (args.len() == 3 && args[2] == "whinge")) {
+        usage(&args[0]);
+        return Err(BAD_COMMAND_LINE_ARGS);
+    }
+
+    config = &args[1].clone();
+    
+    if args.len() == 3 && args[2] == "whinge" {
+        WHINGE.store(true, Ordering::SeqCst);
+    }
+
+     Ok(())
+}
+
+
+
+fn main() -> Result<(), u8> {
+    
+      let config = &args[1]
     println!("Processing config file: {}", config_file);
     // got the config.txt next going to parse it thorugh another module func
     let name = 
@@ -22,5 +44,4 @@ fn main() -> Result<(), u8> {
 
 
     println!("Hello, world!");
-    return Ok(())
 }
